1700d99db6526367ba41778be648d230be882a6f
Borislav Atanasov <b_atanasov@hotmail.com>
2019-02-21 14:10:47 +0000

    Updates

---
+++ precache-manifest.2b8d4183506593990763e22741543152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "798e141f8c4b739265a6",
+    "url": "/brew-shop/static/css/main.40a1d58c.chunk.css"
+  },
+  {
+    "revision": "798e141f8c4b739265a6",
+    "url": "/brew-shop/static/js/main.798e141f.chunk.js"
+  },
+  {
+    "revision": "6286449930b609af4a71",
+    "url": "/brew-shop/static/js/runtime~main.62864499.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/brew-shop/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "fb29026ecc6d34ef517724ae5cb874e4",
+    "url": "/brew-shop/index.html"
+  }
+];
